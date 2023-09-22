63da7bfbd9b84aa7593ac16cd4796a671aa4807f
Déri Gergely <drigrgly@gmail.com>
2023-09-22 23:24:27 +0200

    Started making static pages

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+//routing
+
+app.get("/", (req, res, next) => {
+  res.redirect("/views/notes.html");
+});
+
+
+let server = app.listen(3000, () => {
+  console.log("Running on :3000");
+});
